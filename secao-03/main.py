7c6ad07b1339b7142f5ce44387971b44617d1df5
Julio H Moura <juliohmoura@pm.me>
2022-07-27 16:19:34 -0300

    Método GET

--- secao-03/main.py
+++ secao-03/main.py
@@ -15,7 +15,20 @@ cursos = {
     }
 }
 
+#Endpoints
+
+@app.get('/cursos')
+async def get_cursos():
+    return cursos
+
+@app.get('/cursos/{curso_id}')
+async def get_curso_id(curso_id: int):
+    curso = cursos[curso_id]
+    #curso.update({"id": curso_id})
+
+    return curso
+
 if __name__=='__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=6000, debug=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=6000, debug=True, reload=True)
